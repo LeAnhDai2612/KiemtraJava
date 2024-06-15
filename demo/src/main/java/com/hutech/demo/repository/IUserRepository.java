d30e8acd566e265f66858df20597f6a260708004
LeAnhDai2612 <daihoa0302@gmail.com>
2024-06-15 14:23:32 +0700

    code

---
+++ demo/src/main/java/com/hutech/demo/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.hutech.demo.repository;
+
+import com.hutech.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
